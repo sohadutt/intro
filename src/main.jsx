f8d18f6f9bafc6fba3a70ed23948ff351d0dabcf
sohadutt <sohadutt@users.noreply.github.com>
2025-08-01 21:20:29 +0530

    it wasnt compatible :/

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <SpeedInsights/>
   </StrictMode>,
 )
